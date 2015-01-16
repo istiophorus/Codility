2e14d8b40d34a8456c58ed100beb97b2bbcf6f42
istiophorus <rskotak@gmail.com>
2015-01-16 23:55:42 +0100

    Added solution of Brackets task.

---
+++ Codility.Training.Tests/BracketsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Training.Tests
+{
+	[TestClass]
+	public sealed class BracketsTest
+	{
+		private readonly Brackets _target = new Brackets();
+
+		[TestMethod]
+		public void TestParentheses()
+		{
+			Assert.AreEqual(1, _target.Solve("{}"));
+
+			Assert.AreEqual(1, _target.Solve("[]"));
+
+			Assert.AreEqual(1, _target.Solve("()"));
+
+			Assert.AreEqual(1, _target.Solve("([])"));
+
+			Assert.AreEqual(0, _target.Solve("([)]"));
+
+			Assert.AreEqual(0, _target.Solve("("));
+
+			Assert.AreEqual(0, _target.Solve("[]("));
+		}
+	}
+}
+
+
---
+++ Codility.Training/Brackets.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Training
+{
+	/// <summary>
+	/// my solution of codility Brackets task
+	/// https://codility.com/demo/take-sample-test/brackets
+	/// with results
+	/// https://codility.com/demo/results/demoBPMYF4-ZVZ/
+	/// </summary>
+	public sealed class Brackets
+	{
+		public Int32 Solve(String input)
+		{
+			if (null == input)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				return 1;
+			}
+
+			Char[] chars = input.ToCharArray();
+
+			Stack<Char> stack = new Stack<Char>();
+
+			Dictionary<Char, Char> map = new Dictionary<Char, Char>()
+			{
+				{'{', '}'},
+				{'[', ']'},
+				{'(', ')'}
+			};
+
+			for (Int32 q = 0; q < chars.Length; q++)
+			{
+				Char current = chars[q];
+
+				if (current == '{' || current == '[' || current == '(')
+				{
+					stack.Push(current);
+				}
+				else if (current == '}' || current == ']' || current == ')')
+				{
+					if (stack.Count >= 1)
+					{
+						Char topItem = stack.Peek();
+
+						if (current == map[topItem])
+						{
+							stack.Pop();
+						}
+						else
+						{
+							return 0;
+						}
+					}
+					else
+					{
+						return 0;
+					}
+				}
+			}
+
+			if (stack.Count > 0)
+			{
+				return 0;
+			}
+
+			return 1;
+		}
+	}
+}
