fbd30083df00cd74fc99e65e69c712dd051c5dbf
istiophorus <rskotak@gmail.com>
2015-01-16 23:36:47 +0100

    Added solution of CountDiv.

---
+++ Codility.Training.Tests/CountDivTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Training.Tests
+{
+	[TestClass]
+	public sealed class CountDivTests
+	{
+		private readonly CountDiv _target = new CountDiv();
+
+		[TestMethod]
+		public void Test01()
+		{
+			Assert.AreEqual(1, _target.Solve(5, 6, 5));
+
+			Assert.AreEqual(1, _target.Solve(5, 7, 5));
+
+			Assert.AreEqual(3, _target.Solve(6, 11, 2));
+
+			Assert.AreEqual(1, _target.Solve(4, 6, 3));
+
+			Assert.AreEqual(3, _target.Solve(6, 13, 3));
+
+			Assert.AreEqual(2, _target.Solve(1, 6, 3));
+
+			Assert.AreEqual(1, _target.Solve(6, 14, 5));
+
+			Assert.AreEqual(2, _target.Solve(6, 15, 5));
+
+			Assert.AreEqual(2, _target.Solve(6, 19, 5));
+
+			Assert.AreEqual(2, _target.Solve(7, 15, 5));
+
+			Assert.AreEqual(1, _target.Solve(0, 1, 11));
+
+			Assert.AreEqual(1, _target.Solve(10, 10, 5));
+		}
+	}
+}
+
+
+
+
+
---
+++ Codility.Training/CountDiv.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Training
+{
+	public sealed class CountDiv
+	{
+		/// <summary>
+		/// my solution of the codility task:
+		/// https://codility.com/demo/take-sample-test/count_div
+		/// with results
+		/// https://codility.com/demo/results/demo2ZCSZK-UTQ/
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name="k"></param>
+		/// <returns></returns>
+		public Int32 Solve(Int32 a, Int32 b, Int32 k)
+		{
+			if (a < 0)
+			{
+				throw new ArgumentOutOfRangeException("a");
+			}
+
+			if (b < 0)
+			{
+				throw new ArgumentOutOfRangeException("b");
+			}
+
+			if (k < 1)
+			{
+				throw new ArgumentOutOfRangeException("k");
+			}
+
+			if (a > b)
+			{
+				throw new ArgumentOutOfRangeException("a");
+			}
+
+			Int32 rangeLength = b - a + 1;
+
+			if (k == 1)
+			{
+				return rangeLength;
+			}
+
+			if (rangeLength % k == 0) /// if range length is a multiple of k 
+			{
+				return rangeLength / k;
+			}
+			else
+			{
+				if (a % k == 0)
+				{
+					return rangeLength / k + 1;
+				}
+
+				if (a % k + rangeLength % k> k)
+				{
+					return rangeLength / k + 1;
+				}
+				else
+				{
+					return rangeLength / k;
+				}
+			}
+		}
+	}
+}
