d85d6378d3a3ba459d1ab22db310ea95429994b2
istiophorus <rskotak@gmail.com>
2015-01-30 20:29:23 +0100

    Added solution of the task ChocolatesByNumbers.

---
+++ Codility.Training.Tests/ChocolatesByNumbersTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Training.Tests
+{
+	[TestClass]
+	public sealed class ChocolatesByNumbersTests
+	{
+		private readonly ChocolatesByNumbers _target = new ChocolatesByNumbers();
+
+		[TestMethod]
+		public void SampleChockolateTest()
+		{
+			Int32 result = _target.Solve(10, 4);
+
+			Assert.AreEqual(5, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest06()
+		{
+			Int32 result = _target.Solve(9, 4);
+
+			Assert.AreEqual(9, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest07()
+		{
+			Int32 result = _target.Solve(10, 12);
+
+			Assert.AreEqual(5, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest09()
+		{
+			Int32 result = _target.Solve(1, 2);
+
+			Assert.AreEqual(1, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest01()
+		{
+			Int32 result = _target.Solve(6, 3);
+
+			Assert.AreEqual(2, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest02()
+		{
+			Int32 result = _target.Solve(6, 1);
+
+			Assert.AreEqual(6, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest03()
+		{
+			Int32 result = _target.Solve(6, 6);
+
+			Assert.AreEqual(1, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest04()
+		{
+			Int32 result = _target.Solve(6, 7);
+
+			Assert.AreEqual(6, result);
+		}
+
+		[TestMethod]
+		public void ChockolateTest05()
+		{
+			Int32 result = _target.Solve(6, 8);
+
+			Assert.AreEqual(3, result);
+		}
+
+		[TestMethod]
+		public void LargestCommonDivisorTest01()
+		{
+			Int64 result = ChocolatesByNumbers.LargestCommonDivisor(8, 6);
+
+			Assert.AreEqual(2, result);
+		}
+
+		[TestMethod]
+		public void LeastCommonMultiplicationTest01()
+		{
+			Int64 result = ChocolatesByNumbers.LeastCommonMultiplication(2, 3);
+
+			Assert.AreEqual(6, result);
+		}
+
+		[TestMethod]
+		public void LeastCommonMultiplicationTest02()
+		{
+			Int64 result = ChocolatesByNumbers.LeastCommonMultiplication(15, 10);
+
+			Assert.AreEqual(30, result);
+		}
+
+	}
+}
---
+++ Codility.Training/ChocolatesByNumbers.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Training
+{
+	/// <summary>
+	/// my solution of the codility task:
+	/// https://codility.com/demo/take-sample-test/chocolates_by_numbers
+	/// with results
+	/// https://codility.com/demo/results/demoX7HBP9-4NJ/
+	/// </summary>
+	public sealed class ChocolatesByNumbers
+	{
+		public Int32 Solve(Int32 n, Int32 m)
+		{
+			return (Int32)SolveImpl(n, m);
+		}
+
+		private Int64 SolveImpl(Int64 n, Int64 m)
+		{
+			Console.WriteLine("{0} {1}", n, m);
+
+			if (n <= 0)
+			{
+				throw new ArgumentOutOfRangeException("n");
+			}
+
+			if (m <= 0)
+			{
+				throw new ArgumentOutOfRangeException("m");
+			}
+
+			if (n >= m)
+			{
+				if (n % m == 0)
+				{
+					return n / m;
+				}
+				else
+				{
+					Int64 mult = (Int64)LeastCommonMultiplication(n, m);
+
+					return (mult / m);
+				}
+			}
+			else
+			{
+				if (m % n == 0)
+				{
+					return 1;
+				}
+				else
+				{
+					return SolveImpl(n, m % n);
+				}
+			}
+		}
+
+		public static Int64 LargestCommonDivisor(Int64 a, Int64 b)
+		{
+			if (a <= 0)
+			{
+				throw new ArgumentOutOfRangeException("a");
+			}
+
+			if (b <= 0)
+			{
+				throw new ArgumentOutOfRangeException("b");
+			}
+
+			if (a == b)
+			{
+				return a;
+			}
+
+			if (a > b)
+			{
+				if (a % b == 0)
+				{
+					return b;
+				}
+
+				return LargestCommonDivisor(a % b, b);
+			}
+			else
+			{
+				if (b % a == 0)
+				{
+					return a;
+				}
+
+				return LargestCommonDivisor(b % a, a);
+			}
+		}
+
+		public static Int64 LeastCommonMultiplication(Int64 a, Int64 b)
+		{
+			if (a <= 0)
+			{
+				throw new ArgumentNullException("a");
+			}
+
+			if (b <= 0)
+			{
+				throw new ArgumentOutOfRangeException("b");
+			}
+
+			if (a == b)
+			{
+				return a;
+			}
+
+			if (a >= b)
+			{
+				if (a % b == 0)
+				{
+					return a;
+				}
+				else
+				{
+					return (Int64)a * (Int64)b / (Int64)LargestCommonDivisor(a, b);
+				}
+			}
+			else
+			{
+				return LeastCommonMultiplication(b, a);
+			}
+		}
+	}
+}
