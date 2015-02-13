81a5e2c1f99db10a6168fd588ad2772d65b35567
istiophorus <rskotak@gmail.com>
2015-02-13 22:52:31 +0100

    Added solution of the task GenomicRangeQuery.

---
+++ Codility.Training.Tests/GenomicRangeQueryTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Training.Tests
+{
+	[TestClass]
+	public sealed class GenomicRangeQueryTest
+	{
+		private readonly GenomicRangeQuery _target = new GenomicRangeQuery();
+
+		[TestMethod]
+		public void TestNull()
+		{
+			Int32[] result = _target.Solve("ACGT", null, null);
+
+			Assert.IsNotNull(result);
+
+			Assert.AreEqual(0, result.Length);
+		}
+
+		[TestMethod]
+		public void TestEmpty()
+		{
+			Int32[] result = _target.Solve("ACGT", new Int32[0], new Int32[0]);
+
+			Assert.IsNotNull(result);
+
+			Assert.AreEqual(0, result.Length);
+		}
+
+		[TestMethod]
+		public void SampleTest()
+		{
+			Int32[] result = _target.Solve("CAGCCTA", new Int32[] { 2, 5, 0 }, new Int32[] { 4, 5, 6});
+
+			Assert.IsNotNull(result);
+
+			Int32[] expected = new Int32[] { 2, 4, 1 };
+
+			Assert.AreEqual(3, result.Length);
+
+			for (Int32 q = 0; q < result.Length; q++)
+			{
+				Assert.AreEqual(expected[q], result[q]);
+			}
+		}
+
+		[TestMethod]
+		public void SampleTest2()
+		{
+			Int32[] result = _target.Solve("CAGCCTA", new Int32[] { 0, 0, 0, 2 }, new Int32[] { 0, 1, 2, 6 });
+
+			Assert.IsNotNull(result);
+
+			Int32[] expected = new Int32[] { 2, 1, 1, 1 };
+
+			Assert.AreEqual(4, result.Length);
+
+			for (Int32 q = 0; q < result.Length; q++)
+			{
+				Assert.AreEqual(expected[q], result[q]);
+			}
+		}
+	}
+}
---
+++ Codility.Training/GenomicRangeQuery.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Training
+{
+	/// <summary>
+	/// my solution of the task GenomicRangeQuery
+	/// https://codility.com/demo/take-sample-test/genomic_range_query
+	/// and here are results
+	/// https://codility.com/demo/results/demoPSXTCR-WK2/
+	/// </summary>
+	public sealed class GenomicRangeQuery
+	{
+		private static Int32[] EmptyResult = new Int32[0];
+
+		public Int32[] Solve(String input, Int32[] p, Int32[] w)
+		{
+			if (null == input)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				throw new ArgumentException("input");
+			}
+
+			if (null == p && null == w)
+			{
+				return EmptyResult;
+			}
+
+			if (p.Length != w.Length)
+			{
+				throw new ArgumentOutOfRangeException("w");
+			}
+
+			if (p.Length == 0 && w.Length == 0)
+			{
+				return EmptyResult;
+			}
+
+			Char[] chars = input.ToCharArray();
+
+			//////////////////////////////////////////////////////////////////////////////////////
+			//// prepare summaries
+			////
+
+			Int32[,] summaries = new Int32[chars.Length, 4];
+
+			for (Int32 q = 0; q < chars.Length; q++)
+			{
+				Char current = chars[q];
+
+				if (q > 0)
+				{
+					summaries[q, 0] = summaries[q - 1, 0];
+					summaries[q, 1] = summaries[q - 1, 1];
+					summaries[q, 2] = summaries[q - 1, 2];
+					summaries[q, 3] = summaries[q - 1, 3];
+				}
+
+				summaries[q, GetIndex(current)]++;
+			}
+
+			Int32[] result = new Int32[p.Length];
+
+			Int32[] currentDiff = new Int32[4];
+
+			for (Int32 q = 0; q < result.Length; q++)
+			{
+				Int32 indexP = p[q];
+
+				Int32 indexW = w[q];
+
+				if (indexP < 0 || indexP >= chars.Length)
+				{
+					throw new ArgumentOutOfRangeException("p " + q);
+				}
+
+				if (indexW < 0 || indexW >= chars.Length)
+				{
+					throw new ArgumentOutOfRangeException("w " + q);
+				}
+
+				for (Int32 ix = 0; ix < 4; ix++)
+				{
+					currentDiff[ix] = summaries[indexW, ix] - summaries[indexP, ix];
+				}
+
+				currentDiff[GetIndex(chars[indexP])]++;
+
+				for (Int32 ix = 0; ix < 4; ix++)
+				{
+					if (currentDiff[ix] > 0)
+					{
+						result[q] = ix + 1;
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static Int32 GetIndex(Char ch)
+		{
+			switch (ch)
+			{
+				case 'a':
+				case 'A':
+					return 0;
+
+				case 'c':
+				case 'C':
+					return 1;
+
+				case 'g':
+				case 'G':
+					return 2;
+
+				case 't':
+				case 'T':
+					return 3;
+
+				default:
+					throw new ArgumentOutOfRangeException("ch");
+			}
+		}
+
+		private static Int32 GetImpactFactor(Char ch)
+		{
+			switch (ch)
+			{
+				case 'a':
+				case 'A':
+					return 1;
+
+				case 'c':
+				case 'C':
+					return 2;
+
+				case 'g':
+				case 'G':
+					return 3;
+
+				case 't':
+				case 'T':
+					return 4;
+
+				default:
+					throw new ArgumentOutOfRangeException("ch");
+			}
+		}
+	}
+}
