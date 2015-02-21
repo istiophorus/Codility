44c9d491c0ea92662814987f7d524a5ab8828778
istiophorus <rskotak@gmail.com>
2015-02-21 12:21:24 +0100

    Added solution of Dominator task.

---
+++ Codility.Training.Tests/DominatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.Training.Tests
+{
+	[TestClass]
+	public sealed class DominatorTests
+	{
+		private readonly Dominator _target = new Dominator();
+
+		[TestMethod]
+		public void Test01()
+		{
+			Int32 index = _target.Solve(new Int32[] { 3, 4, 3, 2, 3, -1, 3, 3 });
+
+			Assert.AreEqual(7, index);
+		}
+
+		[TestMethod]
+		public void TestEmpty()
+		{
+			Int32 index = _target.Solve(new Int32[] { });
+
+			Assert.AreEqual(-1, index);
+		}
+
+		[TestMethod]
+		public void TestSingle()
+		{
+			Int32 index = _target.Solve(new Int32[] { 1 });
+
+			Assert.AreEqual(0, index);
+		}
+
+		[TestMethod]
+		public void TestNoDominator()
+		{
+			Int32 index = _target.Solve(new Int32[] { 1, 2, 3, 4});
+
+			Assert.AreEqual(-1, index);
+		}
+
+		[TestMethod]
+		public void TestNoDominator2()
+		{
+			Int32 index = _target.Solve(new Int32[] { 1, 2, 2, 4 });
+
+			Assert.AreEqual(-1, index);
+		}
+	}
+}
---
+++ Codility.Training/Dominator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Training
+{
+	/// <summary>
+	/// my solution of the task Dominator
+	/// https://codility.com/demo/take-sample-test/dominator
+	/// and here are results
+	/// https://codility.com/demo/results/demoCJF6R7-673/
+	/// </summary>
+	public sealed class Dominator
+	{
+		public Int32 Solve(Int32[] input)
+		{
+			if (null == input)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			if (input.Length <= 0)
+			{
+				return -1;
+			}
+
+			if (input.Length == 1)
+			{
+				return 0;
+			}
+
+			Dictionary<Int32, Int32> counters = new Dictionary<Int32, Int32>();
+
+			Int32 currentMaxCount = 0;
+
+			Int32 currentMaxItem = 0;
+
+			Int32 lastMaxIndex = -1;
+
+			for (Int32 q = 0; q < input.Length; q++)
+			{
+				Int32 current = input[q];
+
+				Int32 itemCounter = 0;
+
+				counters.TryGetValue(current, out itemCounter);
+
+				itemCounter++;
+
+				counters[current] = itemCounter;
+
+				if (itemCounter > currentMaxCount)
+				{
+					currentMaxCount = itemCounter;
+
+					currentMaxItem = current;
+
+					lastMaxIndex = q;
+				}
+			}
+
+			Int32 half = input.Length / 2;
+
+			if (currentMaxCount > half)
+			{
+				return lastMaxIndex;
+			}
+			
+
+			return -1;
+		}
+	}
+}
